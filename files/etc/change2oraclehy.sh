e99891ab5ba422f035254be1e01476b049d35e89
laye0619 <laye0619@gmail.com>
2024-09-28 10:07:28 +0800

    trim a bit

--- files/etc/change2oraclehy.sh
+++ files/etc/change2oraclehy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-sed -i "/config routing_node 'oracle_node'/,/^$/ { /option node/ s/'.*'/'oracle_hysteria'/ }" /etc/config/homeproxy
+sed -i "/config routing_node 'oracle_node'/,/^$/ { /option node/ s/'.*'/'ORACLE_Hysteria2'/ }" /etc/config/homeproxy
 /etc/init.d/homeproxy restart
